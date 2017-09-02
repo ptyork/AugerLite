ff4bfb38da0710495b9ca7d76da4de3ba946b56f
Paul York <paul@yorkfamily.com>
2017-09-02 16:17:30 -0400

    Big cleanup effort. Now using Bower and Grunt for all client-side libraries. Shouldn't actually need the /lib folder since this is created and managed by Grunt (via Bower), but it's not hurting anything for now.

--- AugerLite/App_Start/BundleConfig.cs
+++ AugerLite/App_Start/BundleConfig.cs
@@ -5,26 +5,61 @@ namespace Auger
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/scripts/core").Include(
+                "~/lib/jquery/jquery.js",
+                "~/lib/bootstrap/js/bootstrap.js",
+                "~/lib/bootstrap-switch/js/bootstrap-switch.js",
+                "~/lib/mustache.js/mustache.js",
+                "~/Scripts/auger-ui.js"
+            ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+            bundles.Add(new StyleBundle("~/styles/core-light").Include(
+                "~/lib/font-awesome/css/font-awesome.css",
+                "~/Content/bootstrap-light.css",
+                "~/lib/bootstrap-switch/css/bootstrap3/bootstrap-switch.css",
+                "~/Content/auger.css",
+                "~/Content/auger-light.css"
+            ));
+            bundles.Add(new StyleBundle("~/styles/core-dark").Include(
+                "~/lib/font-awesome/css/font-awesome.css",
+                "~/Content/bootstrap-dark.css",
+                "~/lib/bootstrap-switch/css/bootstrap3/bootstrap-switch.css",
+                "~/Content/auger.css",
+                "~/Content/auger-dark.css"
+            ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/scripts/jqueryval").Include(
+                "~/lib/jquery-validation/dist/jquery.validate.js",
+                "~/lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js"
+            ));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/scripts/compat").Include(
+                "~/lib/modernizr/modernizr.js",
+                "~/lib/respond/src/respond.js",
+                "~/lib/es6-promise/es6-promise.auto.js"
+            ));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/scripts/datepicker").Include(
+                "~/lib/moment/moment.js",
+                "~/lib/datetimepicker/js/bootstrap-datetimepicker.js"
+            ));
+            bundles.Add(new StyleBundle("~/styles/datepicker").Include(
+                "~/lib/datetimepicker/css/bootstrap-datetimepicker.css"
+            ));
+
+            bundles.Add(new ScriptBundle("~/scripts/editor").Include(
+                "~/lib/ace/ace.js"
+            ));
+            bundles.Add(new ScriptBundle("~/scripts/ide").Include(
+                "~/lib/ace/ext-language_tools.js",
+                "~/lib/ace/ext-settings_menu.js",
+                "~/Scripts/auger-filemanager.js"
+            ));
         }
     }
 }
---
+++ AugerLite/Gruntfile.js
@@ -0,0 +1,91 @@
+﻿/// <binding BeforeBuild='copy' />
+/*
+This file in the main entry point for defining grunt tasks and using grunt plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
+*/
+module.exports = function (grunt) {
+  var modernizrTests = [
+    "contextmenu",
+    "cookies",
+    "cors",
+    "customevent",
+    "hiddenscroll",
+    "ie8compat",
+    "inputtypes",
+    [
+      "progressbar",
+      "meter"
+    ],
+    "promises",
+    "filereader",
+    "filesystem",
+    "fileinput",
+    "sandbox",
+    "seamless",
+    "srcdoc",
+    "urlparser"
+  ];
+  var modernizrOptions = [
+    "setClasses"
+  ];
+  grunt.initConfig({
+    modernizr: {
+      dev: {
+        "dest": "lib/modernizr/modernizr.js",
+        "crawl": false,
+        "customTests": [],
+        "tests": modernizrTests,
+        "options": modernizrOptions,
+        "uglify": false
+      },
+      dist: {
+        "dest": "lib/modernizr/modernizr.min.js",
+        "crawl": false,
+        "customTests": [],
+        "tests": modernizrTests,
+        "options": modernizrOptions,
+        "uglify": true
+      }
+    },
+    copy: {
+      bower: {
+        files: [
+          {
+            cwd: 'bower_components/', src: [
+              'chai/chai.js',
+              'chai-jquery/chai-jquery.js',
+              'es6-promise/*.js',
+              'jquery-validation-unobtrusive/*js',
+              'mocha/mocha.js',
+              'mustache.js/*.js',
+              'srcdoc-polyfill/srcdoc*.js',
+            ], dest: 'lib/', expand: true
+          },
+          { cwd: 'bower_components/ace-builds/src-min/', src: '**', dest: 'lib/ace/', expand: true },
+          { cwd: 'bower_components/bootstrap/dist/', src: '**', dest: 'lib/bootstrap/', expand: true },
+          { cwd: 'bower_components/bootstrap-switch/dist/', src: '**', dest: 'lib/bootstrap-switch/', expand: true },
+          { cwd: 'bower_components/eonasdan-bootstrap-datetimepicker/build/', src: '**', dest: 'lib/datetimepicker/', expand: true },
+          { cwd: 'bower_components/eonasdan-bootstrap-datetimepicker/src/js', src: 'bootstrap-datetimepicker.js', dest: 'lib/datetimepicker/js/', expand: true },
+          { cwd: 'bower_components/font-awesome/', src: ['css/**', 'fonts/**'], dest: 'lib/font-awesome/', expand: true },
+          { cwd: 'bower_components/jquery/dist/', src: '**', dest: 'lib/jquery/', expand: true },
+          { cwd: 'bower_components/jquery-validation/dist/', src: '**', dest: 'lib/jquery-validation/', expand: true },
+          { cwd: 'bower_components/moment/min/', src: '*.js', dest: 'lib/moment/', expand: true },
+          { cwd: 'bower_components/moment/', src: '*.js', dest: 'lib/moment/', expand: true },
+          { cwd: 'bower_components/moment-timezone/builds/', src: '*.js', dest: 'lib/moment-timezone/', expand: true },
+          { cwd: 'bower_components/moment-timezone/', src: '*.js', dest: 'lib/moment-timezone/', expand: true },
+          { cwd: 'bower_components/respond/dest/', src: '*.js', dest: 'lib/respond/', expand: true },
+        ]
+        //src: [
+        //  'ace-builds/**',
+        //  'bootstrap/dist/**',
+        //  'bootstrap-switch/dist/**',
+        //  'chai/chai.js',
+        //  'chai-jquery/chai-jquery.js'
+
+        //]
+      }
+    }
+  });
+  grunt.loadNpmTasks("grunt-modernizr");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+};
